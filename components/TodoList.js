b45ee3ad9a5a3425990c76b8ebffc4fc863609e2
Genesis <echo_girl@outlook.com>
2020-12-02 14:30:37 -0430

    app Todo T2T202

---
+++ components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// COMPONENT
+import TodoItem from './TodoItem';
+
+const TodoList = ({ todoItems, onRemoveTodo, onToggleTodoDone, onEditTodo, setCustomError }) => {
+  return (
+    <ul>
+      {
+        todoItems && // Check if todoItems exists
+        Array.isArray(todoItems) && // Check if it's an array
+        todoItems.length > 0 && // The array should not be empty
+        todoItems.map(({ id, todo, isDone }) => ( // If all conditions are met, we render a list of todo items
+          <TodoItem
+            key={id}s
+            id={id}
+            todo={todo}
+            onRemoveTodo={onRemoveTodo}
+            onToggleTodoDone={onToggleTodoDone}
+            onEditTodo={onEditTodo}
+            isDone={isDone}
+            setCustomError={setCustomError}
+          />
+        ))
+      }
+    </ul>
+   )
+};
+
+export default React.memo(TodoList); 
